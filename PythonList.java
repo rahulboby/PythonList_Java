d339ab2a7bcd7aed2e4a0c7a292a679a07057184
Your GitHub Username <your_email@example.com>
2025-11-26 10:14:53 +0530

    Update PythonList.java 26-11-2025 included 5+ methods

--- PythonList.java
+++ PythonList.java
@@ -1,12 +1,13 @@
 package PythonList_Java;
 
+
 class Node{
 	Node(Object data){
 		this.data = data;
 	}
 	Object data;
 	Node next;
-	Node prev;
+//	Node prev;
 }
 
 
@@ -15,6 +16,24 @@ public class PythonList {
 	Node head = null;
 	Node last = null;
 	Node dummy = new Node(null);
+	
+	public void append(Object data) {
+		
+		// Create new Node to add into list
+		Node newNode = new Node(data);
+		
+		if(head == null) {
+			head = last = newNode;
+			dummy.next = head;
+		}
+		else {
+			last.next = newNode;
+			last = newNode;
+		}
+		
+		length++;
+		
+	}
 	
 	public Object get(int index) {
 		//Empty List
@@ -48,24 +67,6 @@ public class PythonList {
 			ptr = ptr.next;
 		}
 		return ptr.data;
-		
-	}
-	
-	public void append(Object data) {
-		
-		// Create new Node to add into list
-		Node newNode = new Node(data);
-		
-		if(head == null) {
-			head = last = newNode;
-			dummy.next = head;
-		}
-		else {
-			last.next = newNode;
-			last = newNode;
-		}
-		
-		length++;
 		
 	}
 	
@@ -264,6 +265,7 @@ public class PythonList {
 	}
 	
 	public int index(Object data) {
+		
 		int index = 0;
 		
 		Node ptr = head;
@@ -276,6 +278,26 @@ public class PythonList {
 		}
 		
 		System.err.println("ValueError: list.index(x): x not in list");
+		return Integer.MIN_VALUE;
+	}
+	
+	public int index(Object data, int start, int end) {
+		// (int start = 0, end = len(list)) -> In a range of the list
+		Node ptr = head; int i = 0;
+		//Iterate to start
+		// i already initialised
+		for (; i<start && ptr!=null; i++, ptr = ptr.next);;
+		
+		//Traverse to the end while checking for value
+		while(ptr!=null && i<end && i<length) {
+			if(ptr.data.equals(data)) {
+				return i;
+			}
+			ptr = ptr.next;
+			i++;
+		}
+		
+		System.err.print("ValueError: list.index(x): x not in list ");
 		return Integer.MIN_VALUE;
 	}
 	
@@ -294,6 +316,222 @@ public class PythonList {
 		for(int i = length-1; i>=0; i--, ptr=ptr.next) {
 			ptr.data = listElements[i];
 		}
+	}
+	
+	public void sort() {
+		//Sorts the list in place (modifies the original list). key can be used for custom sorting, and reverse=True sorts in descending order.
+		//In-place sorting of list when elements are comparable
+		
+		// ADD ALL VALUES TO ARRAY TO SORT
+		Node ptr = head;
+		double[] arr = new double[length];
+		int i = 0;
+		while (ptr!=null) {
+			if(!isNumericType(ptr.data)) {
+				System.err.println("TypeError: '<' not supported between instances of 'str' and 'int'");
+				return;
+				
+			}
+			arr[i++] = ((Number) ptr.data).doubleValue();
+			ptr = ptr.next;
+		}
+		
+		// SORT THE ARRAY in ascending order - Insertion Sort
+		// (To be improved with dual-pivot quick sort in the future)
+		for(i = 0; i<length; i++) {
+			int j = i;
+			while(j>0 && arr[j]<arr[j-1]) {
+				double temp = arr[j-1]; 
+				arr[j-1] = arr[j];
+				arr[j] = temp;
+				j--;
+			}
+		}
+		
+		// ADD ALL VALUES BACK TO LIST
+		ptr = head; i = 0;
+		
+		while(ptr!=null) {
+			ptr.data = arr[i++];
+			ptr = ptr.next;
+		}
+		
+		//PROBLEM: All the values are converted to double
+	}
+	
+	public void sort(boolean reverse) {
+		//If ascending or not - reverse=true -> descending
+		//Sorts the list in place (modifies the original list). key can be used for custom sorting, and reverse=True sorts in descending order.
+		
+		
+		// ADD ALL VALUES TO ARRAY TO SORT
+		Node ptr = head;
+		double[] arr = new double[length];
+		int i = 0;
+		while (ptr!=null) {
+			if(!isNumericType(ptr.data)) {
+				System.err.println("TypeError: '<' not supported between instances of 'str' and 'int'");
+				return;
+				
+			}
+			arr[i++] = ((Number) ptr.data).doubleValue();
+			ptr = ptr.next;
+		}
+		
+		// SORT THE ARRAY in ascending order - Insertion Sort
+		// (To be improved with dual-pivot quick sort in the future)
+		for(i = 0; i<length; i++) {
+			int j = i;
+			while(j>0 && arr[j]<arr[j-1]) {
+				double temp = arr[j-1]; 
+				arr[j-1] = arr[j];
+				arr[j] = temp;
+				j--;
+			}
+		}
+		
+		// ADD ALL VALUES BACK TO LIST
+		// If reverse, then add in reverse - for descending order
+		if(reverse) {
+			ptr = head; i = length-1;
+			
+			while(ptr!=null) {
+				ptr.data = arr[i--];
+				ptr = ptr.next;
+			}
+		}
+		else {
+			ptr = head; i = 0;
+			
+			while(ptr!=null) {
+				ptr.data = arr[i++];
+				ptr = ptr.next;
+			}
+		}
+		
+		//PROBLEM: All the values are converted to double
+	}
+	
+	public PythonList sorted(boolean reverse) {
+		// implements sorted(list) -> Returns a new sorted list
+		//If ascending or not - reverse=true -> descending
+		//Sorts the list in place (modifies the original list). key can be used for custom sorting, and reverse=True sorts in descending order.
+		
+		PythonList newList = new PythonList();
+		
+		// ADD ALL VALUES TO ARRAY TO SORT
+		Node ptr = head;
+		double[] arr = new double[length];
+		int i = 0;
+		while (ptr!=null) {
+			if(!isNumericType(ptr.data)) {
+				System.err.println("TypeError: '<' not supported between instances of 'str' and 'int'");
+				return null;
+				
+			}
+			arr[i++] = ((Number) ptr.data).doubleValue();
+			ptr = ptr.next;
+		}
+		
+		// SORT THE ARRAY in ascending order - Insertion Sort
+		// (To be improved with dual-pivot quick sort in the future)
+		for(i = 0; i<length; i++) {
+			int j = i;
+			while(j>0 && arr[j]<arr[j-1]) {
+				double temp = arr[j-1]; 
+				arr[j-1] = arr[j];
+				arr[j] = temp;
+				j--;
+			}
+		}
+		
+		// ADD ALL VALUES TO NEW LIST
+		// If reverse, then add in reverse - for descending order
+		if(reverse) {
+			i = length-1;
+			
+			while(i>=0) {
+				Node newNode = new Node(arr[i--]);
+				if(newList.head == null) {
+					newList.dummy.next = newList.head = newList.last = newNode;
+				}
+				else {
+					newList.last.next = newNode;
+					newList.last = newNode;
+				}
+			}
+		}
+		
+		else {
+			i = 0;
+			
+			while(i<length) {
+				Node newNode = new Node(arr[i++]);
+				if(newList.head == null) {
+					newList.dummy.next = newList.head = newList.last = newNode;
+				}
+				else {
+					newList.last.next = newNode;
+					newList.last = newNode;
+				}
+			}
+		}
+		
+		//PROBLEM: All the values are converted to double
+		return newList;
+	}
+	
+	public PythonList sorted() {
+		// implements sorted(list) -> Returns a new sorted list
+		//If ascending or not - reverse=true -> descending
+		//Sorts the list in place (modifies the original list). key can be used for custom sorting, and reverse=True sorts in descending order.
+		
+		PythonList newList = new PythonList();
+		
+		// ADD ALL VALUES TO ARRAY TO SORT
+		Node ptr = head;
+		double[] arr = new double[length];
+		int i = 0;
+		while (ptr!=null) {
+			if(!isNumericType(ptr.data)) {
+				System.err.println("TypeError: '<' not supported between instances of 'str' and 'int'");
+				return null;
+				
+			}
+			arr[i++] = ((Number) ptr.data).doubleValue();
+			ptr = ptr.next;
+		}
+		
+		// SORT THE ARRAY in ascending order - Insertion Sort
+		// (To be improved with dual-pivot quick sort in the future)
+		for(i = 0; i<length; i++) {
+			int j = i;
+			while(j>0 && arr[j]<arr[j-1]) {
+				double temp = arr[j-1]; 
+				arr[j-1] = arr[j];
+				arr[j] = temp;
+				j--;
+			}
+		}
+		
+		// ADD ALL VALUES TO NEW LIST
+		// If reverse, then add in reverse - for descending order
+		
+		i = 0;
+		
+		while(i<length) {
+			Node newNode = new Node(arr[i++]);
+			if(newList.head == null) {
+				newList.dummy.next = newList.head = newList.last = newNode;
+			}
+			else {
+				newList.last.next = newNode;
+				newList.last = newNode;
+			}
+		}
+		
+		//PROBLEM: All the values are converted to double
+		return newList;
 	}
 	
 	public PythonList reversed() {
@@ -353,6 +591,82 @@ public class PythonList {
 		
 	}
 	
+	public double max() {
+		// implements max(list)
+		/*
+			Traceback (most recent call last):
+			  File "<pyshell#3>", line 1, in <module>
+			    max(lst)
+			TypeError: '>' not supported between instances of 'str' and 'int'
+		 */
+		
+		Node ptr = head;
+		
+		// Initialise max value
+		double max = Double.MIN_VALUE;
+		
+		while(ptr!=null) {
+			if(!isNumericType(ptr.data)) {
+				System.err.println("TypeError: '>' not supported between instances of 'str' and 'int'");
+				return Integer.MIN_VALUE;
+			}
+			
+			double doubleValue = ((Number) ptr.data).doubleValue();
+			if(doubleValue > max )
+				max = doubleValue;
+			ptr = ptr.next;
+
+		}
+	
+		return max;
+	}
+	
+	public double min() {
+		// implements min(list)
+		
+		Node ptr = head;
+		
+		// Initialise max value
+		double min = Double.MAX_VALUE;
+		
+		while(ptr!=null) {
+			if(!isNumericType(ptr.data)) {
+				System.err.println("TypeError: '<' not supported between instances of 'str' and 'int'");
+				return Integer.MIN_VALUE;
+			}
+			
+			double doubleValue = ((Number) ptr.data).doubleValue();
+			if(doubleValue < min)
+				min = doubleValue;
+			ptr = ptr.next;
+
+		}
+	
+		return min;
+	}
+	
+	public double sum() {
+		// implements sum(list)
+		
+		Node ptr = head;
+		
+		// Initialise max value
+		double sum = 0;
+		
+		while(ptr!=null) {
+			if(!isNumericType(ptr.data)) {
+				System.err.println("TypeError: '>' not supported between instances of 'str' and 'int'");
+				return Integer.MIN_VALUE;
+			}
+			
+			double doubleValue = ((Number) ptr.data).doubleValue();
+			sum += doubleValue;
+			ptr = ptr.next;
+		}
+	
+		return sum;
+	}
+	
 
 	public int length() {
 		return length;
@@ -364,20 +678,27 @@ public class PythonList {
 		head = last = dummy.next = null;
 	}
 	
+	protected boolean isNumericType(Object obj) {
+		// Used to find if the object is numeric
+		// Used in the methods min, max, sum, sort, sort(boolean).
+		return obj instanceof Integer ||
+				obj instanceof Double ||
+				obj instanceof Float ||
+				obj instanceof Long	 ||
+				obj instanceof Character;
+	}
+	
+	
+	
 	// ---------------------------------------------------------------------
 	// ------------------------- FUTURE ADDITIONS --------------------------
 	// ---------------------------------------------------------------------
 
-	// * SORTING with incomparable datatypes
 	// * Throwing EXCEPTIONS instead of error printing using System.err.print()
 	// * Code is not threadsafe. Any concurrent access will cause corrupted state (race conditions, partial updates, etc.).
 	// * Implement Doubly Linked linked - efficient back-traversal
+	// * [DONE] SORTING with incomparable datatypes
 	
-
-	
-	private void sort() {
-		//In-place sorting of list when elements are comparable
-	}
 	
 	private void append(PythonList adding_list) {
 		//Shallow Copy - WORKING ON IT
@@ -388,5 +709,5 @@ public class PythonList {
 /*
  ___MISTAKES I DID ___
  * 
- * using != for wrapper classes in remove() method - use .equals() instead - != checks for reference not value
+ * using != for wrapper classes in remove() method - use .equals() instead - '!=' checks for reference not value
  */
